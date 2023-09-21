f0417892e4a673ef6ddda973662dc472b093fe81
rapharrrr <125589553+rapharrrr@users.noreply.github.com>
2023-09-21 17:23:58 +0100

    small accessibility improvements

--- app/components/FooterColumn.jsx
+++ app/components/FooterColumn.jsx
@@ -2,18 +2,20 @@ import Link from "next/link";
 
 const FooterColumn = ({ title, links }) => (
     <div className="footer_column">
-        <h4 className="font-semibold text-uphold-darker sm:mb-4">{title}</h4>
+        <p className="font-semibold text-uphold-darker sm:mb-4">{title}</p>
         <ul className="flex flex-col gap-2 font-normal">
             {links.map((link) => (
-                <Link
-                    href={link.href} 
-                    key={link.text} 
-                    target="_blank" 
-                    rel="noopener noreferrer" 
-                    className="hover:underline"
-                >
-                    {link.text}
-                </Link>
+                <li>
+                    <Link
+                        href={link.href} 
+                        key={link.text} 
+                        target="_blank" 
+                        rel="noopener noreferrer" 
+                        className="hover:underline"
+                        >
+                        {link.text}
+                    </Link>
+                </li>
             ))}
         </ul>
     </div>
