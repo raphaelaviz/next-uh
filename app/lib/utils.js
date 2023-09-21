eb3c897d5df1e7f093ac602d4c0cb5364c7b112f
rapharrrr <125589553+rapharrrr@users.noreply.github.com>
2023-09-21 14:11:48 +0100

    revalidate cache

--- app/lib/utils.js
+++ app/lib/utils.js
@@ -1,7 +1,9 @@
 'use server'
 
 export async function getCurrencies(baseCurrency) {
-    const response = await fetch(`https://api.uphold.com/v0/ticker/${baseCurrency}`);
+    const response = await fetch(`https://api.uphold.com/v0/ticker/${baseCurrency}`, 
+      { next: { revalidate: 2 } }
+    );
     if (!response.ok) {
       throw new Error('Failed to fetch data');
     }
