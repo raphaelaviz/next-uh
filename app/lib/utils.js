c408ed5063de85b41b181d513d83e4e892a83a41
rapharrrr <125589553+rapharrrr@users.noreply.github.com>
2023-09-21 14:08:04 +0100

    default 'forced cache" on fetch API

--- app/lib/utils.js
+++ app/lib/utils.js
@@ -1,9 +1,7 @@
 'use server'
 
 export async function getCurrencies(baseCurrency) {
-    const response = await fetch(`https://api.uphold.com/v0/ticker/${baseCurrency}`, {
-      cache: 'no-cache',
-    });
+    const response = await fetch(`https://api.uphold.com/v0/ticker/${baseCurrency}`);
     if (!response.ok) {
       throw new Error('Failed to fetch data');
     }
