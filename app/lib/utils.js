95500e9e65867d3c930ded39c2d9481528c713e6
rapharrrr <125589553+rapharrrr@users.noreply.github.com>
2023-09-21 14:32:08 +0100

    revalidate tag option for caching

--- app/lib/utils.js
+++ app/lib/utils.js
@@ -1,8 +1,12 @@
 'use server'
 
+import { revalidateTag } from "next/cache";
+
 export async function getCurrencies(baseCurrency) {
-    const response = await fetch(`https://api.uphold.com/v0/ticker/${baseCurrency}`, 
+  revalidateTag('main')
     
+  const response = await fetch(`https://api.uphold.com/v0/ticker/${baseCurrency}`, 
+      { next: { tags: ['main'] } }
     );
     if (!response.ok) {
       throw new Error('Failed to fetch data');
