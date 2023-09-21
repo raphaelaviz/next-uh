b97368d9c2d332324cb588c5fddc5834e67bf5d8
rapharrrr <125589553+rapharrrr@users.noreply.github.com>
2023-09-21 14:21:06 +0100

    default cache option (test)

--- app/lib/utils.js
+++ app/lib/utils.js
@@ -2,7 +2,7 @@
 
 export async function getCurrencies(baseCurrency) {
     const response = await fetch(`https://api.uphold.com/v0/ticker/${baseCurrency}`, 
-      { next: { revalidate: 2 } }
+    
     );
     if (!response.ok) {
       throw new Error('Failed to fetch data');
